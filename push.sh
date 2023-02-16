79b42016477bd907be0bbf9cc6e8cc1f3003a092
gre4bee <gre4be+1@gmail.com>
2023-02-16 12:49:10 +0800

    2023-02-16

--- push.sh
+++ push.sh
@@ -1,11 +1,11 @@
-export GIT_AUTHOR_NAME="Botkaze"
-export GIT_AUTHOR_EMAIL="gkpyon+1@gmail.com"
+export GIT_AUTHOR_NAME="gre4bee"
+export GIT_AUTHOR_EMAIL="gre4be+1@gmail.com"
 export GIT_COMMITTER_NAME="$GIT_AUTHOR_NAME"
 export GIT_COMMITTER_EMAIL="$GIT_AUTHOR_EMAIL"
 
 git add .
 TZ=Asia/Tokyo git commit -m "$(date +%Y-%m-%d)"
-git push git@github.com:kcwiki/cache.git master
+git push git@github.com:gre4bee/cache.git master
 
 # TODO: ???
 
