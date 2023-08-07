62ce2a917e356ea65b0ed704f291e723016f34fd
gre4bee <1538175+gre4bee@users.noreply.github.com>
2023-08-07 22:09:23 +0800

    amend email

--- GadgetServerConsole/Program.cs
+++ GadgetServerConsole/Program.cs
@@ -56,7 +56,7 @@ void GitPush()
 	DateTime now = DateTime.UtcNow + new TimeSpan(9, 0, 0);
 	string gitCommand = "git";
 	string gitAddArgument = "add -A";
-	string gitCommitArgument = $"commit -m \"{now:dd-mm-yyyy}\" --author=\"gre4bee <1538175+gre4bee@users.noreply.github.com>\"";
+	string gitCommitArgument = $"commit -m \"{now:dd-mm-yyyy}\" --author=\"gre4bee <11538175+gre4bee@users.noreply.github.com>\"";
 	string gitPushArgument = "push origin dev";
 
 	Process.Start(gitCommand, gitAddArgument);
