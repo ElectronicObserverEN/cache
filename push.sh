c4ca9e7f14ae5f0e182ddca24b6484e3afb7e532
gre4bee <1538175+gre4bee@users.noreply.github.com>
2023-08-07 12:54:04 +0800

    Revert "Removed extra files and update exe"
    This reverts commit 968e8076bb29b43b9a1bd34fb59ccc38209b633c.
    
    # Conflicts:
    #	GadgetServerConsole.exe

---
+++ push.sh
@@ -0,0 +1,17 @@
+export GIT_AUTHOR_NAME="gre4bee"
+export GIT_AUTHOR_EMAIL="gre4be+1@gmail.com"
+export GIT_COMMITTER_NAME="$GIT_AUTHOR_NAME"
+export GIT_COMMITTER_EMAIL="$GIT_AUTHOR_EMAIL"
+
+git add .
+TZ=Asia/Tokyo git commit -m "$(date +%Y-%m-%d)"
+git push origin master
+
+# TODO: ???
+
+# rm -f /tmp/MyCache.zip
+# mkdir -p /tmp/MyCache/KanColle
+# cp -r gadget_html5/ html/ kcscontents/ /tmp/MyCache/KanColle/
+# cd /tmp
+# zip -qr MyCache.zip MyCache/
+# rm -rf /tmp/MyCache
