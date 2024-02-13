645ef4acc6ab61c4f8e35b2b4dbdbae506bdee61
MyAngelKamikaze <myangelkamikaze@gmail.com>
2024-02-13 18:11:54 +0900

    2024/02/13

--- gadget_html5/js/kcs_const.js
+++ gadget_html5/js/kcs_const.js
@@ -59,7 +59,7 @@ MaintenanceInfo.EndDateTime   = Date.parse("2024/02/13 00:59:59");
 
 // Version 情報
 var VersionInfo = {};
-VersionInfo.scriptVesion      = "5.8.2.1";
+VersionInfo.scriptVesion      = "5.8.3.0";
 
 // Entrance 情報
 var EntranceInfo = {};
